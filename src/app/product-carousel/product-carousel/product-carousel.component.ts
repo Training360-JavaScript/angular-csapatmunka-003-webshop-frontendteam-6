6851aa57ad3f2cfef0be5e580ed846fbe5237e10
JellinekGy <90341733+JellinekGy@users.noreply.github.com>
2022-01-25 19:21:26 +0100

    Product carousel using  product card

--- src/app/product-carousel/product-carousel/product-carousel.component.ts
+++ src/app/product-carousel/product-carousel/product-carousel.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ProductCardComponent } from '';
+import { ProductCardComponent } from 'src/app/common/product-card/product-card.component';
 
 @Component({
   selector: 'app-product-carousel',
@@ -7,7 +7,9 @@ import { ProductCardComponent } from '';
   styleUrls: ['./product-carousel.component.scss']
 })
 export class ProductCarouselComponent implements OnInit {
+
   @Input product!: ProductCardComponent;
+
   constructor() { }
 
   ngOnInit(): void {
